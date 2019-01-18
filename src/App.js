0ab42818f758b54a837460e61cb4d9cb4d9f975c
Adam Rizzo <adamrizzo@Adams-MacBook-Pro.local>
2019-01-18 09:29:11 -0500

    Finished components, added router and working links

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'
 import logo from './logo.svg';
-import About from './components/About/About'
-import Contact from './components/Contact/Contact'
 
 // import CSS files
 //import './styles/header/header.css'
@@ -10,19 +9,26 @@ import './App.css';
 // import components
 import Header from './shared/Header'
 import Footer from './shared/Footer'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
+import Portfolio from './components/Portfolio/Portfolio'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <div className="container">
-          <div className="row">
-            <Contact />
+      <BrowserRouter>
+        <div className="App">
+          <Header />
+          <div className="container">
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/portfolio" component={Portfolio} />
+            <Route exact path="/contact" component={Contact} />
           </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </BrowserRouter>
     );
   }
 }
--- src/shared/Header.js
+++ src/shared/Header.js
@@ -1,24 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () =>
   <nav className="navbar navbar-expand-lg navbar-dark">
-    <a className="navbar-brand" href="#">AJR Designs</a>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <div className="navbar-brand">AJR Designs</div>
+    <button className="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav mr-auto">
         <li className="nav-item">
-          <a className="nav-link" href="#">Home</a>
+          <Link className="nav-link" to="/">Home</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">About</a>
+          <Link className="nav-link" to="/about">About</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Resume</a>
+          <Link className="nav-link" to="/portfolio">Portfolio</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Contact</a>
+          <Link className="nav-link" to="/contact">Contact</Link>
         </li>
       </ul>
     </div>
