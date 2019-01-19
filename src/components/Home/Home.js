4c78a71f460c275b0983bb5fad6e52a66ca573e5
Adam Rizzo <adamrizzo@Adams-MacBook-Pro.local>
2019-01-19 13:30:30 -0500

    added img to attempt background

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
 const Home = () =>
-  <div>
-    <h1 className="welcome">Welcome to the home of</h1>
-    <br />
-    <img id='logo' src='/img/AJR_Designs.png' />
-    <br />
-    <h1 className="welcome">where my goal is to give you and your business or personal interest a website to call home!</h1>
+  <div id='home-component'>
+    <div id='home-component-background'>
+      <h1 className="welcome">Welcome to the home of</h1>
+      <br />
+      <img id='logo' src='/img/AJR_Designs.png' />
+      <br />
+      <h1 className="welcome">where my goal is to give you and your business or personal interest a website to call home!</h1>
+    </div>
   </div>
 
   export default Home
