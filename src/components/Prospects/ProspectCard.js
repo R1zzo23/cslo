9ddc148cda353f65cff1cf75ad74055426e7d6cb
Adam Rizzo <adamrizzo@Adams-MacBook-Pro.local>
2019-01-21 13:45:09 -0500

    example draft class is now alphabetical, added prospect components

---
+++ src/components/Prospects/ProspectCard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import BasicInfo from './BasicInfo'
+import PersonalityTraits from './PersonalityTraits'
+
+const ProspectCard = () =>
+  <div>
+    <h1>Prospect Card</h1>
+  </div>
+
+  export default ProspectCard
