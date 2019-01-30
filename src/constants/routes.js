c1c38adaf7dba150dcc4634c37818ebf9b2513b4
Adam Rizzo <arizzo.dev@gmail.com>
2019-01-30 09:03:55 -0500

    added sign up page

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,7 +1,9 @@
 export const HOME = '/';
-export const LOG_IN = '/login';
 export const ACCOUNT = '/account';
 export const ADMIN = '/admin';
 export const DRAFT_CLASS = '/draftclass';
 export const SEND_SCOUTS = '/sendscouts';
 export const PROSPECT = '/prospect/:id';
+export const SIGN_UP = '/signup';
+export const LOG_IN = '/login';
+export const PASSWORD_FORGET = '/pw-forget';
