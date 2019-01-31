db016cc4ca90247017df36df4d21957515a54b59
Adam Rizzo <arizzo.dev@gmail.com>
2019-01-31 13:47:06 -0500

    removed comments from firebase.js and added to a .txt file

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -16,21 +16,6 @@ class Firebase {
     app.initializeApp(config);
 
     this.auth = app.auth();
-
-    // *** Auth API ***
-
-    //doCreateUserWithEmailAndPassword = (email, password) =>
-    //  this.auth.createUserWithEmailAndPassword(email, password);
-
-    //doSignInWithEmailAndPassword = (email, password) =>
-    //  this.auth.signInWithEmailAndPassword(email, password);
-
-    //doSignOut = () => this.auth.signOut();
-
-    //doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
-
-    //doPasswordUpdate = password =>
-    //  this.auth.currentUser.updatePassword(password);
   }
 }
 
