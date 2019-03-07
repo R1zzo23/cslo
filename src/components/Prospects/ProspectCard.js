f9837478c93359a8844af55f634fb1aaf0423966
Adam Rizzo <arizzo.dev@gmail.com>
2019-03-07 13:08:01 -0500

    added CSV export with CSS changes

--- src/components/Prospects/ProspectCard.js
+++ src/components/Prospects/ProspectCard.js
@@ -13,40 +13,30 @@ export class ProspectCard extends React.Component {
     const prospect = class24[i];
 
     return (
-        <div>
-          <div className='row'>
-            <div className='col-md-12'>
-              <BasicInfo prospect={prospect} />
-            </div>
-          </div>
-          <br />
-          <div className='row justify-content-center'>
-            <div className='col-md-3 col-sm-12'>
-              <PersonalityTraits prospect={prospect} />
-            </div>
-            <div className='col-md-3 col-sm-12'>
-              <ShootingAbility prospect={prospect} />
-            </div>
-            <div className='col-md-3 col-sm-12'>
-              <BallActions prospect={prospect} />
-            </div>
-          </div>
-          <div className='row skill-row'>
-            <div className='col-md-12'>
-              <SkillRatings prospect={prospect} />
-            </div>
-          </div>
-          <br />
-          <div className='row'>
-            <h4>Developer Notes:</h4>
-            <ul>
-              <li>Skill rating table [very bottom] is currently populated with actual Ratings</li>
-              <li>Idea is skill rating table will add new row for each scout you send for this player</li>
-              <li>Every scout will add new row with deviations from scouting report</li>
-              <li>Idea: if scouts achieve highest and lowest possible deviation, true rating will be revealed</li>
-            </ul>
+      <div>
+        <div className='row'>
+          <div className='col-md-12'>
+            <BasicInfo prospect={prospect} />
           </div>
         </div>
+        <br />
+        <div className='row justify-content-center'>
+          <div className='col-md-3 col-sm-12'>
+            <PersonalityTraits prospect={prospect} />
+          </div>
+          <div className='col-md-3 col-sm-12'>
+            <ShootingAbility prospect={prospect} />
+          </div>
+          <div className='col-md-3 col-sm-12'>
+            <BallActions prospect={prospect} />
+          </div>
+        </div>
+        <div className='row skill-row'>
+          <div className='col-md-12'>
+            <SkillRatings prospect={prospect} />
+          </div>
+        </div>
+      </div>
     );
   }
 };
--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import '@firebase/firestore'
 import { withFirebase } from '../Firebase'
+import { CSVLink, CSVDownload } from "react-csv"
 
 const TeamPage = ({firebase}) => (
   <div>
@@ -13,7 +14,62 @@ class Team extends React.Component{
   constructor(props: IProps) {
     super(props);
     this.state = {
-      data: []
+      data: [],
+      abrev: "",
+      team: "",
+      fileName: "",
+      headers: [
+        { label: "First", key: "FirstName" },
+        { label: "Last", key: "LastName" },
+        { label: "POS", key: "Position"},
+        { label: "AGE", key: "Age"},
+        { label: "HT", key: "DisplayHeight"},
+        { label: "WT", key: "Weight"},
+        { label: "FROM", key: "College"},
+        { label: "DUNK RATE", key: "DunkRate"},
+        { label: "RIM AREA RATE", key: "RARate"},
+        { label: "DriveKick", key: "DriveKick"},
+        { label: "DriveShot", key: "DriveShot"},
+        { label: "PostUp", key: "PostUp"},
+        { label: "CS", key: "CS"},
+        { label: "PASS", key: "Pass"},
+        { label: "LocATB", key: "LocATB"},
+        { label: "LocCorner", key: "LocCorner"},
+        { label: "LodMid", key: "LocMidrange"},
+        { label: "LocPaint", key: "LocPaint"},
+        { label: "FG_RA", key: "FG_RA"},
+        { label: "FG_RA_POT", key: "FG_RA_POT"},
+        { label: "FG_ITP", key: "FG_ITP"},
+        { label: "FG_ITP_POT", key: "FG_ITP_POT" },
+        { label: "FG_MID", key: "FG_MID"},
+        { label: "FG_MID_POT", key: "FG_MID_POT"},
+        { label: "FG_COR", key: "FG_COR"},
+        { label: "FG_COR_POT", key: "FG_COR_POT"},
+        { label: "FT", key: "FT"},
+        { label: "FT_POT", key: "FT_POT"},
+        { label: "SCR", key: "Scoring"},
+        { label: "SCR_POT", key: "Scoring_POT"},
+        { label: "PAS", key: "Passing"},
+        { label: "PAS_POT", key: "Passing_POT"},
+        { label: "HDL", key: "Handling"},
+        { label: "HDL_POT", key: "Handling_POT"},
+        { label: "ORB", key: "OReb"},
+        { label: "ORB_POT", key: "OReb_POT"},
+        { label: "DRB", key: "DReb"},
+        { label: "DRB_POT", key: "DReb_POT"},
+        { label: "BLK", key: "Block"},
+        { label: "BLK_POT", key: "Block_POT"},
+        { label: "STL", key: "Steal"},
+        { label: "STL_POT", key: "Steal_POT"},
+        { label: "DRFL", key: "DrawFoul"},
+        { label: "DRFL_POT", key: "DrawFoul_POT"},
+        { label: "DEF", key: "Defender"},
+        { label: "DEF_POT", key: "Defender_POT"},
+        { label: "DIS", key: "Discipline"},
+        { label: "DIS_POT", key: "Discipline_POT"},
+        { label: "IQ", key: "BballIQ"},
+        { label: "IQ_POT", key: "BballIQ_POT"}
+      ]
     };
   }
   componentDidMount() {
@@ -24,6 +80,18 @@ class Team extends React.Component{
     const db = fire.auth.app.firebase_.firestore();
 
     let scouts = [];
+
+    db.collection("franchises").where("email", "==", userEmail)
+    .get()
+    .then((docSnapshot) => {
+      docSnapshot.forEach((doc) => {
+        this.setState({
+          abrev: doc.data().abrev,
+          team: doc.data().team,
+          fileName: doc.data().abrev + '-scouts'
+        });
+      });
+    });
 
     db.collection("scouts").where("Email", "==", userEmail)
     .get()
@@ -42,31 +110,55 @@ class Team extends React.Component{
   render(){
     const scoutTable = (
       <div>
-        <table className="table">
-          <thead className="thead-dark">
+        <CSVLink data={this.state.data}
+                 filename={this.state.fileName}
+                 className="btn btn-primary"
+                 target="_blank"
+                 headers={this.state.headers}>
+          Export Scouts to CSV
+        </CSVLink>
+        <table className="table table-sm table-striped">
+          <thead>
             <tr>
-              <th scope="col">FIRST</th>
-              <th scope="col">LAST</th>
-              <th scope="col">POS</th>
-              <th scope="col">DunkRate</th>
-              <th scope="col">RARate</th>
-              <th scope="col">RIM</th>
-              <th scope="col">PAINT</th>
-              <th scope="col">MID</th>
-              <th scope="col">COR</th>
-              <th scope="col">ATB</th>
-              <th scope="col">FT</th>
-              <th scope="col">SCR</th>
-              <th scope="col">PAS</th>
-              <th scope="col">HDL</th>
-              <th scope="col">ORB</th>
-              <th scope="col">DRB</th>
-              <th scope="col">BLK</th>
-              <th scope="col">STL</th>
-              <th scope="col">DRFL</th>
-              <th scope="col">DEF</th>
-              <th scope="col">DIS</th>
-              <th scope="col">IQ</th>
+              <th className="player-info" colspan="3">PLAYER</th>
+              <th className="dunk-rim-rates" colspan="2">RATES</th>
+              <th className="ball-actions" colspan="5">BALL ACTIONS</th>
+              <th className="floor-locations" colspan="4">FLOOR LOCATIONS</th>
+              <th className="shot-ratings" colspan="6">SHOT RATINGS</th>
+              <th className="other-ratings" colspan="11">OTHER RATINGS</th>
+            </tr>
+            <tr>
+              <th className="player-info" scope="col">FIRST</th>
+              <th className="player-info" scope="col">LAST</th>
+              <th className="player-info" scope="col">POS</th>
+              <th className="dunk-rim-rates" scope="col">DUNK</th>
+              <th className="dunk-rim-rates" scope="col">RIM</th>
+              <th className="ball-actions" scope="col">DriveKick</th>
+              <th className="ball-actions" scope="col">DriveShoot</th>
+              <th className="ball-actions" scope="col">PostUp</th>
+              <th className="ball-actions" scope="col">CS</th>
+              <th className="ball-actions" scope="col">Pass</th>
+              <th className="floor-locations" scope="col">ATB</th>
+              <th className="floor-locations" scope="col">COR</th>
+              <th className="floor-locations" scope="col">MID</th>
+              <th className="floor-locations" scope="col">PAINT</th>
+              <th className="shot-ratings" scope="col">RIM</th>
+              <th className="shot-ratings" scope="col">PAINT</th>
+              <th className="shot-ratings" scope="col">MID</th>
+              <th className="shot-ratings" scope="col">COR</th>
+              <th className="shot-ratings" scope="col">ATB</th>
+              <th className="shot-ratings" scope="col">FT</th>
+              <th className="other-ratings" scope="col">SCR</th>
+              <th className="other-ratings" scope="col">PAS</th>
+              <th className="other-ratings" scope="col">HDL</th>
+              <th className="other-ratings" scope="col">ORB</th>
+              <th className="other-ratings" scope="col">DRB</th>
+              <th className="other-ratings" scope="col">BLK</th>
+              <th className="other-ratings" scope="col">STL</th>
+              <th className="other-ratings" scope="col">DRFL</th>
+              <th className="other-ratings" scope="col">DEF</th>
+              <th className="other-ratings" scope="col">DIS</th>
+              <th className="other-ratings" scope="col">IQ</th>
             </tr>
           </thead>
           <tbody>
@@ -77,6 +169,15 @@ class Team extends React.Component{
                 <td>{scout.Position}</td>
                 <td>{scout.DunkRate}</td>
                 <td>{scout.RARate}</td>
+                <td>{scout.DriveKick}</td>
+                <td>{scout.DriveShot}</td>
+                <td>{scout.PostUp}</td>
+                <td>{scout.CS}</td>
+                <td>{scout.Pass}</td>
+                <td>{scout.LocATB}</td>
+                <td>{scout.LocCorner}</td>
+                <td>{scout.LocMidrange}</td>
+                <td>{scout.LocPaint}</td>
                 <td>{scout.FG_RA}/{scout.FG_RA_POT}</td>
                 <td>{scout.FG_ITP}/{scout.FG_ITP_POT}</td>
                 <td>{scout.FG_MID}/{scout.FG_MID_POT}</td>
