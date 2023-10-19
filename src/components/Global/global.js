1ec439f7013d5c9f55707620d7b0781044f99892
Adam Rizzo <arizzo.dev@gmail.com>
2023-10-19 10:34:48 -0400

    Updates

--- src/components/Global/global.js
+++
@@ -1,8 +0,0 @@
-global.First = 'Sep 9th';
-global.Second = 'TBD';
-global.Third = 'TBD';
-global.Fourth = 'TBD';
-global.Fifth = 'TBD';
-global.Sixth = 'TBD';
-global.Seventh = 'TBD';
-global.Eighth = 'TBD';
