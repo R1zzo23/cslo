dc8d5ee02744ff68a6ca66bb66d2b73ccb97e616
Adam Rizzo <arizzo.dev@gmail.com>
2024-11-05 12:45:42 -0500

    updating again

---
+++ build/precache-manifest.112cdaa8abe5fed2973056f32edb7e43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "21a65fb9d83f4a6896cb",
+    "url": "/cslo/static/js/runtime~main.21a65fb9.js"
+  },
+  {
+    "revision": "1ba8efd3330176b4ed49",
+    "url": "/cslo/static/js/main.1ba8efd3.chunk.js"
+  },
+  {
+    "revision": "ffd519d5ebf0798ec91e",
+    "url": "/cslo/static/js/1.ffd519d5.chunk.js"
+  },
+  {
+    "revision": "1ba8efd3330176b4ed49",
+    "url": "/cslo/static/css/main.fe7941a8.chunk.css"
+  },
+  {
+    "revision": "ffd519d5ebf0798ec91e",
+    "url": "/cslo/static/css/1.a2a93674.chunk.css"
+  },
+  {
+    "revision": "6ee2e6d8817f5cd4cdf4fff8baa4547b",
+    "url": "/cslo/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cslo/precache-manifest.4a3bbfd245a56aa56b8d4b3aed0471e0.js"
+  "/cslo/precache-manifest.112cdaa8abe5fed2973056f32edb7e43.js"
 );
 
 workbox.clientsClaim();
