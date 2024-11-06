8807ec65704bc175562bec67cbf127d391d68957
Adam Rizzo <arizzo.dev@gmail.com>
2024-11-06 14:19:02 -0500

    Updates

--- precache-manifest.dfed05dcb13029fa526212738e90900c.js
+++ precache-manifest.dfed05dcb13029fa526212738e90900c.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/cslo/static/js/runtime~main.21a65fb9.js"
   },
   {
-    "revision": "1ba8efd3330176b4ed49",
-    "url": "/cslo/static/js/main.1ba8efd3.chunk.js"
+    "revision": "2224e007d9e1a170a8ea",
+    "url": "/cslo/static/js/main.2224e007.chunk.js"
   },
   {
     "revision": "ffd519d5ebf0798ec91e",
     "url": "/cslo/static/js/1.ffd519d5.chunk.js"
   },
   {
-    "revision": "1ba8efd3330176b4ed49",
+    "revision": "2224e007d9e1a170a8ea",
     "url": "/cslo/static/css/main.fe7941a8.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/cslo/static/css/1.a2a93674.chunk.css"
   },
   {
-    "revision": "6ee2e6d8817f5cd4cdf4fff8baa4547b",
+    "revision": "14a555b759548448ee22c53ab8ee7240",
     "url": "/cslo/index.html"
   }
 ];
