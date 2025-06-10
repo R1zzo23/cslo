37a90eeee7beb33b697f6c8a406b9dd3acb28dd8
Adam Rizzo <arizzo.dev@gmail.com>
2025-06-10 10:58:39 -0400

    fixed 2034_basic to be alphabetized

---
+++ build/precache-manifest.e86b21fa0724c1feaf63eba3e76d5d64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "21a65fb9d83f4a6896cb",
+    "url": "/cslo/static/js/runtime~main.21a65fb9.js"
+  },
+  {
+    "revision": "6f5498f520b4130ea6f8",
+    "url": "/cslo/static/js/main.6f5498f5.chunk.js"
+  },
+  {
+    "revision": "24bea2b8210958b0cc3c",
+    "url": "/cslo/static/js/1.24bea2b8.chunk.js"
+  },
+  {
+    "revision": "6f5498f520b4130ea6f8",
+    "url": "/cslo/static/css/main.b1699c1f.chunk.css"
+  },
+  {
+    "revision": "24bea2b8210958b0cc3c",
+    "url": "/cslo/static/css/1.3efa6dbb.chunk.css"
+  },
+  {
+    "revision": "a26275b80e01c2be96f57158c51130a5",
+    "url": "/cslo/index.html"
+  }
+];
